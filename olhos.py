e9e0f1e8514e61e80d6178009d79174000ff04a7
Joselino Santos <joselinosantosti@gmail.com>
2018-12-31 19:21:39 -0200

    Update olhos.py

--- olhos.py
+++ olhos.py
@@ -7,8 +7,8 @@ eye_cascade = cv2.CascadeClassifier('xml/haarcascade_eye.xml')
 
 img = cv2.imread('img/lena.jpg')
 cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-rostos = eye_cascade.detectMultiScale(cinza, 1.3, 5)
-for (x,y,w,h) in rostos:
+olhos = eye_cascade.detectMultiScale(cinza, 1.3, 5)
+for (x,y,w,h) in olhos:
 	cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 	roi_gray = cinza[y:y+h, x:x+w]
 	roi_color = img[y:y+h, x:x+w]
